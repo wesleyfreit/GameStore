f2338edd745b4de76c6f659f450cc2b0ae5cd6fc
RICHARD WESLEY BARBOSA FREITAS <wesleyrichard30@gmail.com>
2023-09-28 20:28:22 -0300

    creating swagger docs

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,38 @@
-import express from 'express';
-import sequelize from './db/database';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { isCelebrateError } from 'celebrate';
+import express, { Errback, NextFunction, Request, Response } from 'express';
+import swaggerUi from 'swagger-ui-express';
+
+import swaggerDocs from './config/swagger.json';
+import routes from './routes';
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
 
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use(routes);
+
+app.use((req, res, next) => {
+  res.status(404).send('Content not found');
+});
+
+app.use((err: Errback, req: Request, res: Response, next: NextFunction) => {
+  console.log(err);
+
+  if (isCelebrateError(err)) {
+    let message = err.message;
+    if (Object.fromEntries(err.details).body) {
+      message += ': ' + Object.fromEntries(err.details).body.details[0].message;
+    }
+    return res.status(400).json({ error: message });
+  } else {
+    res.status(500).send('Internal server error');
   }
-})();
+});
 
 app.listen(port, () => {
   console.log(`Server is running in the port ${port}`);
