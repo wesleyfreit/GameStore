7f7568c34ca2af72493e7e289011765931a6741a
Richard Freitas <wesleyrichard30@gmail.com>
2023-11-28 10:07:36 -0300

    adding request games and return it in a flat list

--- mobile/src/screens/home/HomeScreen.tsx
+++ mobile/src/screens/home/HomeScreen.tsx
@@ -1,6 +1,44 @@
-import React from 'react';
-import { Text } from 'react-native';
+import { api } from '@/lib/axios';
+import { isAxiosError } from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Alert, FlatList, Image, SafeAreaView, Text, View } from 'react-native';
 
 export const HomeScreen = () => {
-  return <Text>Home</Text>;
+  const [games, setGames] = useState<IGame[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const request = await api.get('/index');
+        setGames(request.data.games);
+      } catch (error) {
+        if (isAxiosError(error)) {
+          const message = error.response?.data;
+          Alert.alert('Erro', `A tentativa gerou o seguinte erro: ${message}`);
+        }
+      }
+    })();
+  }, []);
+  return (
+    <SafeAreaView style={{ flex: 1, alignItems: 'center', marginTop: 15 }}>
+      <FlatList
+        data={games}
+        renderItem={({ item }) => (
+          <View key={item.id}>
+            <Image
+              source={{
+                uri: 'http://192.168.253.245:8080'.concat(item.imageUrl),
+              }}
+              style={{
+                width: 170,
+                height: 75,
+              }}
+            />
+
+            <Text>{item.title}</Text>
+          </View>
+        )}
+      />
+    </SafeAreaView>
+  );
 };
---
+++ mobile/src/types/game/index.d.ts
@@ -0,0 +1,12 @@
+interface IGame {
+  id: string
+  title: string;
+  year: number;
+  price: number;
+  description: string;
+  disponibility: boolean;
+  genre: string;
+  imageUrl: string;
+}
+
+interface IAdminGame {}
