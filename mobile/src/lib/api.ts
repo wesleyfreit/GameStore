af4eb7571f3812bbded4ed80fb4c2c874480e4ad
RICHARD WESLEY BARBOSA FREITAS <wesleyrichard30@gmail.com>
2023-12-14 16:52:21 -0300

    adding axios-retry to try to make a request more than once

--- mobile/src/lib/api.ts
+++ mobile/src/lib/api.ts
@@ -1,6 +1,9 @@
 import { URL_API } from '@env';
 import axios from 'axios';
+import axiosRetry from 'axios-retry';
 
 export const api = axios.create({
   baseURL: URL_API.concat('/api/v1'),
 });
+
+axiosRetry(api, { retries: 4 });
