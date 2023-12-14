061fd1d8b9b03777393ffb00ed3bd3337a0d6074
Richard Freitas <wesleyrichard30@gmail.com>
2023-12-14 09:09:34 -0300

    adjusting gama create and edit schema

--- mobile/src/schemas/createAndEditGameSchema.ts
+++ mobile/src/schemas/createAndEditGameSchema.ts
@@ -12,17 +12,22 @@ export const createAndEditGameSchema = (
         : yup.string().required('Este campo deve receber um arquivo.'),
     title: yup
       .string()
-      .min(1, 'O nome do jogo deve conter pelo menos 1 caractere.')
+      .min(1, 'O título do jogo deve ser inserido.')
       .max(40)
       .required('Este campo deve ser preenchido.'),
     year: yup
       .number()
-      .min(1950, 'O ano de fabricação não deve ser menor que 1950')
-      .max(2035, 'O ano de fabricação não deve ser maior que 2035')
+      .typeError('O ano de lançamento do jogo deve ser inserido')
+      .min(1950, 'O ano de lançamento não deve ser menor que 1950')
+      .max(2035, 'O ano de lançamento não deve ser maior que 2035')
       .required('Este campo deve ser preenchido.'),
-    price: yup.number().min(0).required('Este campo deve ser preenchido.'),
+    price: yup
+      .number()
+      .typeError('O preço do jogo deve ser inserido')
+      .min(0)
+      .required('Este campo deve ser preenchido.'),
     description: yup.string().required('Este campo deve ser preenchido.'),
     genre: genre ? yup.string() : yup.string().required('Este campo deve ser definido.'),
-    disponibility: yup.boolean(),
+    disponibility: yup.boolean().default(false),
   });
 };
